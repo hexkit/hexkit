a4fe12262aa3ebdd9e2e0231e62ee3fe849cf38d
notaphplover <roberto.pintos.lopez@gmail.com>
2026-01-08 12:55:01 +0100

    chore: add prettier config package

--- packages/backend/patterns/src/index.ts
+++ packages/backend/patterns/src/index.ts
@@ -1,27 +1,13 @@
-import { Writable } from './common/application/models/Writable.js';
-import { Handler } from './common/application/modules/Handler.js';
-import { Wrapper } from './common/domain/models/Wrapper.js';
-import { Builder } from './common/domain/modules/Builder.js';
-import { BuilderAsync } from './common/domain/modules/BuilderAsync.js';
-import { ReportBasedSpec } from './common/domain/modules/ReportBasedSpec.js';
-import { Spec } from './common/domain/modules/Spec.js';
-import {
+export type { Writable } from './common/application/models/Writable.js';
+export type { Handler } from './common/application/modules/Handler.js';
+export type { Wrapper } from './common/domain/models/Wrapper.js';
+export type { Builder } from './common/domain/modules/Builder.js';
+export type { BuilderAsync } from './common/domain/modules/BuilderAsync.js';
+export type { ReportBasedSpec } from './common/domain/modules/ReportBasedSpec.js';
+export type { Spec } from './common/domain/modules/Spec.js';
+export type {
   BaseEither,
   Either,
   Left,
   Right,
 } from './common/domain/patterns/fp/Either.js';
-
-export type {
-  BaseEither,
-  Builder,
-  BuilderAsync,
-  Either,
-  Handler,
-  Left,
-  ReportBasedSpec,
-  Right,
-  Spec,
-  Wrapper,
-  Writable,
-};
